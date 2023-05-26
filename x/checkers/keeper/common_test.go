986a82f7f40fa6e1cbacfd0ab7b118e3274901c8
calin <calinoiu.alexandru@agilfreaks.com>
2023-05-26 21:18:08 +0300

    Add implementation for CreateGame

--- x/checkers/keeper/common_test.go
+++
@@ -1,9 +0,0 @@
-package keeper_test
-
-import "github.com/alice/checkers/x/checkers/testutil"
-
-const (
-	alice = testutil.Alice
-	bob   = testutil.Bob
-	carol = testutil.Carol
-)
--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,20 +1,58 @@
 package keeper_test
 
 import (
+	"context"
+	keepertest "github.com/alice/checkers/testutil/keeper"
+	"github.com/alice/checkers/testutil/sample"
+	"github.com/alice/checkers/x/checkers"
+	"github.com/alice/checkers/x/checkers/keeper"
 	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestGameCreate(t *testing.T) {
-	msgServer, ctx := setupMsgServer(t)
+	msgServer, _, ctx := setupMessageServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(ctx, &types.MsgCreateGame{
-		Creator: alice,
-		Black:   bob,
-		Red:     carol,
+		Creator: sample.AccAddress(),
+		Black:   sample.AccAddress(),
+		Red:     sample.AccAddress(),
 	})
 	require.NoError(t, err)
 	require.EqualValues(t, &types.MsgCreateGameResponse{
 		GameIndex: "1",
 	}, createResponse)
+}
+
+func TestGameCreateGameWasSaved(t *testing.T) {
+	black := sample.AccAddress()
+	red := sample.AccAddress()
+	msgServer, k, ctx := setupMessageServerCreateGame(t)
+	_, err := msgServer.CreateGame(ctx, &types.MsgCreateGame{
+		Creator: sample.AccAddress(),
+		Black:   black,
+		Red:     red,
+	})
+	require.NoError(t, err)
+
+	systemInfo, found := k.GetSystemInfo(sdk.UnwrapSDKContext(ctx))
+	require.True(t, found)
+	require.Equal(t, uint64(2), systemInfo.GetNextId())
+
+	storedGame, found := k.GetStoredGame(sdk.UnwrapSDKContext(ctx), "1")
+	require.True(t, found)
+	require.EqualValues(t, types.StoredGame{
+		Index: "1",
+		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:  "b",
+		Black: black,
+		Red:   red,
+	}, storedGame)
+}
+
+func setupMessageServerCreateGame(t *testing.T) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
 }
